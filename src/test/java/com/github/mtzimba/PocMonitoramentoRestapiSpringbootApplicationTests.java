8a91c5fdb3a40b333e56091f3dfb8aaca2f2683c
Matheus Cardoso <mtzimba@gmail.com>
2021-04-02 21:26:53 -0300

    Spring Boot Admin

---
+++ src/test/java/com/github/mtzimba/PocMonitoramentoRestapiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mtzimba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMonitoramentoRestapiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
